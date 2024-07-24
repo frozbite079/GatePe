426137389093038684a74b75d20e519618395db5
om badhe <ombadhe079@gmail.com>
2024-07-24 12:07:10 +0530

    updating database config

--- GatePe/GatePe/settings.py
+++ GatePe/GatePe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-exvc1ic219*e^l*awnqh+1nxh9v3ey*%7^v(r1sk3k_yrouo01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,13 @@ WSGI_APPLICATION = "GatePe.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "gatepe",
+        'USER':"root",
+        "PASSWORD":"redspark123",
+        "HOST":"localhost",
+        "PORT":'3307'
+        
     }
 }
 
