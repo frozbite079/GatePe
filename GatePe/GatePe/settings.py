aadd686a580a2733bffa02039cd7ccad2c5ebff4
om badhe <ombadhe079@gmail.com>
2024-07-25 10:30:31 +0530

    ok

--- GatePe/GatePe/settings.py
+++ GatePe/GatePe/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import load_dotenvz
 
 load_dotenv()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the  project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
