989da7c108916dc7abd44261ecf14397bfce7dd2
soham1027 <sohamghayal02@gmail.com>
2024-07-25 18:29:41 +0530

    add community

--- GatePe/Community/urls.py
+++ GatePe/Community/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-
+from .views import *
 urlpatterns = [
+    path('community_post/', CommunityPostView.as_view(), name='community_post'),
+    path('notice/', NoticeView.as_view(), name='notice'),
+    path('survey/', SurveyView.as_view(), name='survey'),
+    path('survey_response/', SurveyResponseView.as_view(), name='survey_response'),
+    path('feedback/', FeedbackView.as_view(), name='feedback'),
+   
    
 ]
--- GatePe/Visitors/serializer.py
+++ GatePe/Visitors/serializer.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import *
 from GatePeApp.serializer import *
 class VisitorSerializer(serializers.ModelSerializer):
-    # security_personnel=  ground_provider_profile_id=UserSerializer(many=False)
-     # ground_provider_profile_id= serializers.StringRelatedField(many=False)
+    # security_personnel= UserSerializer(many=True)
+    # security_personnel= serializers.StringRelatedField(many=False)
     class Meta:
         model=Visitor
         fields=('name','contact','location','reason_for_visit','approval_status','visit_time','security_personnel','resident')
